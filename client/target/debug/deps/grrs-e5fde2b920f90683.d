8a8323598b2a98c4a8349bda13c93a3b7c6fd8fc
PatateHunter <ichtitski@gmail.com>
2022-06-07 20:26:36 +0200

    init project

---
+++ client/target/debug/deps/grrs-e5fde2b920f90683.d
@@ -0,0 +1,5 @@
+/home/chtitski_icha/Documents/GitKraken/la-patate-chaude/grrs/target/debug/deps/grrs-e5fde2b920f90683: src/main.rs
+
+/home/chtitski_icha/Documents/GitKraken/la-patate-chaude/grrs/target/debug/deps/grrs-e5fde2b920f90683.d: src/main.rs
+
+src/main.rs:
---
+++ client/target/debug/grrs.d
@@ -0,0 +1,1 @@
+/home/chtitski_icha/Documents/GitKraken/la-patate-chaude/grrs/target/debug/grrs: /home/chtitski_icha/Documents/GitKraken/la-patate-chaude/grrs/src/main.rs
