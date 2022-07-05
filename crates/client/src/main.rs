f3459dd6826488678f738fd58e2d39dfdad2e348
PatateHunter <ichtitski@gmail.com>
2022-07-05 15:21:55 +0200

    Clean

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -6,13 +6,12 @@ use std::io::{Read, Write};
 use std::mem::size_of;
 use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::vec;
-//use crate::ChallengeAnswer::ChallengeAnswer::ChallengeName;
 use serde::{Deserialize, Serialize};
 use serde_json;
 use shared::Models::{ChallengeAnswer, ChallengeValue, message};
 use shared::Models::ChallengeResult::ChallengeResult;
 use shared::Models::message::Message;
-use shared::Models::message::Message::{ PublicLeaderBoard,Subscribe, Welcome};
+use shared::Models::message::Message::{PublicLeaderBoard, Subscribe, Welcome};
 use shared::Models::subscribe::subscribe;
 use shared::Models::welcome::welcome;
 use rand::Rng;
@@ -23,11 +22,9 @@ use shared::Models::MD5HashCashOutput::MD5HashCashOutput;
 use shared::Models::PublicPlayer::PublicPlayer;
 
 fn main() {
-
-
     let playerName = generate_random_string(10);
     let mut tcpStream1 = connect_and_subscribe_player(playerName.clone());
-            /** Round **/
+    /** Round **/
 
 
     loop {
@@ -41,7 +38,7 @@ fn main() {
         }
 
 
-        let other_players = get_other_players_name(&publicLeaderBoard,&playerName);
+        let other_players = get_other_players_name(&publicLeaderBoard, &playerName);
         println!("{:?}", other_players);
         let target = pick_random_player_name(&other_players);
 
@@ -54,25 +51,20 @@ fn main() {
         let mut messageType = get_type(&message);
 
         while messageType == "Challenge" {
-            //if get_type == "Challenge" {
-                processChallenge(&target, &mut tcpStream1, &message);
+            processChallenge(&target, &mut tcpStream1, &message);
 
-                // RoundSummary
-                let roundSummaryLenght = messageLength(&tcpStream1);
-                let roundSummary = readMessage(&tcpStream1, roundSummaryLenght);
-                println!("{:?}", roundSummary);
+            // RoundSummary
+            let roundSummaryLenght = messageLength(&tcpStream1);
+            let roundSummary = readMessage(&tcpStream1, roundSummaryLenght);
+            println!("{:?}", roundSummary);
 
             messageType = get_type(&roundSummary);
             message = roundSummary;
-
-            //}
         }
-
-
     }
 
-       // Err(err) => panic!("Cannot connect : {err}")
-    }
+    // Err(err) => panic!("Cannot connect : {err}")
+}
 
 fn processChallenge(playerName: &String, tcpStream1: &mut TcpStream, message: &Message) {
     let challenge = message;
@@ -134,59 +126,61 @@ fn get_type(dataType: &Message) -> String {
         Message::RoundSummary(_) => {
             "RoundSummary".to_string()
         }
-        _ => {"".to_string()}
+        _ => { "".to_string() }
     }
 }
 
-    fn serializeMessage(msg: &Message) -> String {
-        let serialized = serde_json::to_string(&msg);
-        match serialized {
-            Ok(mut str) => {
-                println!("ok:{str}");
-                str
-            }
-            Err(err) => {
-                panic!("failed");
-            }
+fn serializeMessage(msg: &Message) -> String {
+    let serialized = serde_json::to_string(&msg);
+    match serialized {
+        Ok(mut str) => {
+            println!("ok:{str}");
+            str
+        }
+        Err(err) => {
+            panic!("failed");
         }
     }
-    fn writeMessage(mut stream: &TcpStream, content: &String) {
-        println!("{}", content);
-        let mut messageLenght: u32 = content.len() as u32;
-        let prefix = messageLenght.to_be_bytes();
-        println!("{:?}", prefix);
+}
 
-        let message = content.as_bytes();
+fn writeMessage(mut stream: &TcpStream, content: &String) {
+    println!("{}", content);
+    let mut messageLenght: u32 = content.len() as u32;
+    let prefix = messageLenght.to_be_bytes();
+    println!("{:?}", prefix);
 
-        stream.write_all(&prefix);
-        stream.write_all(&message);
-    }
+    let message = content.as_bytes();
 
-    fn messageLength(mut stream: &TcpStream) -> usize {
-        let mut bufferLenght = [0u8; 4];
-        let res = stream.read_exact(&mut bufferLenght).unwrap();
-        let value = u32::from_be_bytes(bufferLenght);
-        println!("{:?} res", &value);
-        *&value as usize
-    }
-    fn readMessage(mut stream: &TcpStream, messageLenght: usize) -> Message {
-        let mut buffer = vec![0u8; messageLenght];
-        let res = stream.read_exact(&mut buffer);
-        match res {
-            Ok(_) => {
-                println!("{:?}", buffer);
-                let x = String::from_utf8_lossy(&buffer);
-                println!("{:?}dd", x);
-                let data = serde_json::from_str::<Message>(&*x);
-                data.unwrap()
-            }
-            Err(err) => {
-                panic!("{:?}", err);
-            }
+    stream.write_all(&prefix);
+    stream.write_all(&message);
+}
+
+fn messageLength(mut stream: &TcpStream) -> usize {
+    let mut bufferLenght = [0u8; 4];
+    let res = stream.read_exact(&mut bufferLenght).unwrap();
+    let value = u32::from_be_bytes(bufferLenght);
+    println!("{:?} res", &value);
+    *&value as usize
+}
+
+fn readMessage(mut stream: &TcpStream, messageLenght: usize) -> Message {
+    let mut buffer = vec![0u8; messageLenght];
+    let res = stream.read_exact(&mut buffer);
+    match res {
+        Ok(_) => {
+            println!("{:?}", buffer);
+            let x = String::from_utf8_lossy(&buffer);
+            println!("{:?}dd", x);
+            let data = serde_json::from_str::<Message>(&*x);
+            data.unwrap()
+        }
+        Err(err) => {
+            panic!("{:?}", err);
         }
     }
+}
 
-fn get_other_players_name(publicLeaderBoard : &Message, playerToExclude: &String) -> Vec<String> {
+fn get_other_players_name(publicLeaderBoard: &Message, playerToExclude: &String) -> Vec<String> {
     let mut other_players: Vec<String> = vec![];
     let publicLeaderBoard = publicLeaderBoard.clone();
     match publicLeaderBoard {
@@ -200,14 +194,10 @@ fn get_other_players_name(publicLeaderBoard : &Message, playerToExclude: &String
         _ => {
             panic!("Not a PublicLeaderBoard");
         }
-
     }
     other_players
-
-    //result.players.filter(|x| x.name.ne( playerToExclude)).collect()
-    //publicLeaderBoard::<PublicLeaderBoard>().players.iter().filter(|player| player.name != playerToExclude).map(|player| player.clone()).collect()
-
 }
+
 fn pick_random_player_name(player_names: &Vec<String>) -> String {
     let mut rng = rand::thread_rng();
     let index = rng.gen_range(0, player_names.len());
@@ -215,71 +205,65 @@ fn pick_random_player_name(player_names: &Vec<String>) -> String {
 }
 
 
-    // generate random string of length 10
-    fn generate_random_string(len: usize) -> String {
-        let mut rng = rand::thread_rng();
-        let random_string: String = (0..len).map(|_| rng.sample(rand::distributions::Alphanumeric)).collect();
-        random_string
-    }
+// generate random string of length 10
+fn generate_random_string(len: usize) -> String {
+    let mut rng = rand::thread_rng();
+    let random_string: String = (0..len).map(|_| rng.sample(rand::distributions::Alphanumeric)).collect();
+    random_string
+}
 
-    // stays string
-    // seed :decimal += 1;
-    // format : hexadecimal = format!("{:x}", seed);
-    // seed + message
-    // hashcode = md5::compute(format.as_bytes());
-    // hashcode: hexadecimal = format!("{:x}", hashcode);
-    // format hashcode to binary
-    // Determine the number of 0s in the binary representation of the hashcode.
-    // If the number of 0s is > complexity, the hashcode is valid.
-    fn determine_complexity(hashcode: &str) -> u32 {
-        let mut count = 0;
-        for c in hashcode.chars() {
-            if c == '0' {
-                count += 1;
-            }
+
+fn determine_complexity(hashcode: &str) -> u32 {
+    let mut count = 0;
+    for c in hashcode.chars() {
+        if c == '0' {
+            count += 1;
         }
-        count
     }
+    count
+}
 
 
-    fn format_seed_and_message(seed: u32, message: &str) -> String {
-        let format = format!("{:x}", seed);
-        let hashcode = md5::compute(format.as_bytes());
-        let hashcode = format!("{:x}", hashcode);
-        format!("{}{}", hashcode, message)
-    }
-fn generate_hash(complexity :u32,message: &str) -> MD5HashCashOutput{
+fn format_seed_and_message(seed: u32, message: &str) -> String {
+    let format = format!("{:x}", seed);
+    let hashcode = md5::compute(format.as_bytes());
+    let hashcode = format!("{:x}", hashcode);
+    format!("{}{}", hashcode, message)
+}
+
+fn generate_hash(complexity: u32, message: &str) -> MD5HashCashOutput {
     let mut verif = false;
     let mut index = 0;
-    let mut hash:String;
-    let mut seed:String;
-    let mut result:MD5HashCashOutput = MD5HashCashOutput { seed: 0, hashcode: "".to_string() };
-    loop  {
+    let mut hash: String;
+    let mut seed: String;
+    let mut result: MD5HashCashOutput = MD5HashCashOutput { seed: 0, hashcode: "".to_string() };
+    loop {
         seed = create_seed(index);
         println!("seed : {:?} ", seed);
         let elem = format!("{}{}", seed, message);
         // println!("elem : {:?}", elem);
         let hashcode = md5::compute(elem);
         //   println!("seed : {:?} hashcode : {:?} ", seed,hashcode);
-        hash =format!("{:x}", hashcode);
+        hash = format!("{:x}", hashcode);
         // println!("hashcode : {:?}", str);
-        verif=is_hashcode_valid(hash,complexity);
-        if(verif){
+        verif = is_hashcode_valid(hash, complexity);
+        if (verif) {
             //  println!("hashcode : {:?}", hashcode);
-            result.seed= index as u64;
-            result.hashcode=format!("{:x}", hashcode).to_uppercase();
+            result.seed = index as u64;
+            result.hashcode = format!("{:x}", hashcode).to_uppercase();
             break;
         }
-        index=index+1;
+        index = index + 1;
     }
     result
 }
 
-fn create_seed(val: u32)->String{
-    let hexa= format!("{:01$x}", val, 16);
+fn create_seed(val: u32) -> String {
+    let hexa = format!("{:01$x}", val, 16);
     // println!("{}", elem);
     hexa.to_uppercase()
 }
+
 fn convert_to_binary_from_hex(hex: String) -> String {
     let to_binary = hex
         .chars()
@@ -287,24 +271,17 @@ fn convert_to_binary_from_hex(hex: String) -> String {
         .collect();
     to_binary
 }
-/*fn convert_to_binary_from_hex(hex: String) -> String {
-    let to_binary = hex
-        .chars()
-        .map(|c| to_binary(c))
-        .collect();
-    to_binary
-}*/
 
 fn is_hashcode_valid(hashcode: String, complexity: u32) -> bool {
-let mut val_in_binary = convert_to_binary_from_hex(hashcode.to_uppercase());
-println!("hashcode : {:?} val_in_binary : {:?}", hashcode,val_in_binary);
-let mut verif = true;
-for index in 0..complexity {
-if(val_in_binary.chars().nth(index as usize).unwrap() != '0'){
-verif = false;
-}
-}
-verif
+    let mut val_in_binary = convert_to_binary_from_hex(hashcode.to_uppercase());
+    println!("hashcode : {:?} val_in_binary : {:?}", hashcode, val_in_binary);
+    let mut verif = true;
+    for index in 0..complexity {
+        if (val_in_binary.chars().nth(index as usize).unwrap() != '0') {
+            verif = false;
+        }
+    }
+    verif
 }
 
 fn to_binary(c: char) -> String {
@@ -325,39 +302,39 @@ fn to_binary(c: char) -> String {
         'D' => "1101",
         'E' => "1110",
         'F' => "1111",
-        _  => "",
+        _ => "",
     };
     b.to_string()
 }
-    fn connect_and_subscribe_player(name:String) -> TcpStream {
-        let subscribe = Message::Subscribe(subscribe { name: name.parse().unwrap() });
-        //let h = Message::Welcome(Welcome { version: 2 });
+
+fn connect_and_subscribe_player(name: String) -> TcpStream {
+    let subscribe = Message::Subscribe(subscribe { name: name.parse().unwrap() });
 
 
-        let mut tcpStream = TcpStream::connect("localhost:7878");
-        match tcpStream {
-            Ok(mut tcpStream) => {
-                // Hello
-                let helloMessage = Message::Hello;
-                let serializeHM = serializeMessage(&helloMessage);
-                writeMessage(&tcpStream, &serializeHM);
-                // Welcome
-                let welcomeMessageLenght = messageLength(&tcpStream);
-                let welcomeMessage = readMessage(&tcpStream, welcomeMessageLenght);
-                println!("{:?}", welcomeMessage);
-                // Subcription player 1
-                let serializeSubscribe = serializeMessage(&subscribe);
-                writeMessage(&tcpStream, &serializeSubscribe);
-                // SubscribeResult player 1
-                let SubscribeResultLength = messageLength(&tcpStream);
-                let SubscribeResult = readMessage(&tcpStream, SubscribeResultLength);
-                println!("{:?}", SubscribeResult);
-                tcpStream
-            }
-
-            Err(err) => panic!("Cannot connect : {err}")
+    let mut tcpStream = TcpStream::connect("localhost:7878");
+    match tcpStream {
+        Ok(mut tcpStream) => {
+            // Hello
+            let helloMessage = Message::Hello;
+            let serializeHM = serializeMessage(&helloMessage);
+            writeMessage(&tcpStream, &serializeHM);
+            // Welcome
+            let welcomeMessageLenght = messageLength(&tcpStream);
+            let welcomeMessage = readMessage(&tcpStream, welcomeMessageLenght);
+            println!("{:?}", welcomeMessage);
+            // Subcription player 1
+            let serializeSubscribe = serializeMessage(&subscribe);
+            writeMessage(&tcpStream, &serializeSubscribe);
+            // SubscribeResult player 1
+            let SubscribeResultLength = messageLength(&tcpStream);
+            let SubscribeResult = readMessage(&tcpStream, SubscribeResultLength);
+            println!("{:?}", SubscribeResult);
+            tcpStream
         }
+
+        Err(err) => panic!("Cannot connect : {err}")
     }
+}
 
 
 
